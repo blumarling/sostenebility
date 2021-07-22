06b0854329ef3a4ec8b1a8f23a76c8c67ecff436
Gabriele Merra <gabriele.merra@gmail.com>
2021-07-22 17:58:44 +0200

    update gtm

--- pages/_document.js
+++ pages/_document.js
@@ -34,16 +34,16 @@ export default class MyDocument extends Document {
           <Main />
           <NextScript />
           <div id="modals"></div>
-          <script async src="https://www.googletagmanager.com/gtag/js?id=G-MGV16WG54E"></script>
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'G-MGV16WG54E');`
-            }}
-          />
+          <div dangerouslySetInnerHTML={{__html:`
+          <!-- Google Tag Manager (noscript) -->
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=G-MGV16WG54E"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          <!-- End Google Tag Manager (noscript) -->
+          <!-- Google Tag Manager (noscript) -->
+          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=G-MGV16WG54E"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+          <!-- End Google Tag Manager (noscript) -->
+          `}}></div>
         </body>
       </Html>
     )
